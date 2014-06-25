ffd799be7d06f9315918f93ed58d2f8ab298e1ca
Serena Gupta <serenagupta@Serena-Guptas-MacBook-Pro.local>
2014-06-25 11:53:52 -0700

    post comments and dismiss keyboard

--- PaperPlanesV0.2.0/ViewController.h
+++ PaperPlanesV0.2.0/ViewController.h
@@ -11,18 +11,21 @@
 
 @interface ViewController : UIViewController <UIScrollViewDelegate, HPGrowingTextViewDelegate>
 
+@property (strong, nonatomic) IBOutlet UIView *mainView;
+
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIScrollView *imageScrollView;
 
 @property (strong, nonatomic) HPGrowingTextView *textView;
-
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *postCommentHeight;
 @property (weak, nonatomic) IBOutlet UIView *postCommentContainer;
 
-- (IBAction)tapPostComment:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *postButton;
+- (IBAction) tapPostComment:(id) sender;
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardHeight;
 
+@property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *dragPostCommentContainerRecognizer;
+- (IBAction) dragPostCommentContainer:(id) sender;
 
 @end
--- PaperPlanesV0.2.0/ViewController.m
+++ PaperPlanesV0.2.0/ViewController.m
@@ -10,16 +10,29 @@
 #import "PPBoxView.h"
 
 /* Post Comment Constants */
-#define X_COMMENT_OFFSET 7.0f
-#define Y_COMMENT_OFFSET 7.0f
-#define COMMENT_WIDTH 236.0f
-#define COMMENT_MIN_HEIGHT 36.0f
-#define COMMENT_MAX_HEIGHT 77.5f
-#define TEXT_SIZE 15.0f
+//#define X_COMMENT_OFFSET 7.0f
+//#define Y_COMMENT_OFFSET 7.0f
+//#define COMMENT_WIDTH 236.0f
+//#define COMMENT_MIN_HEIGHT 36.0f
+//#define COMMENT_MAX_HEIGHT 77.5f
+//#define TEXT_SIZE 15.0f
+//#define PLACEHOLDER_TEXT @"Give Feedback here..."
+
+#define COMMENT_CONTAINTER_WIDTH 480.0f
+#define COMMENT_CONTAINTER_HEIGHT 45.0f
+#define POST_BUTTON_WIDTH 40.0f
+#define SIDE_MARGIN 10.0f
+
+#define COMMENT_INIT_HEIGHT 37.0f
+#define COMMENT_WIDTH 250.0f
+#define X_COMMENT_OFFSET SIDE_MARGIN
+#define Y_COMMENT_OFFSET 3.0f
+#define TEXT_SIZE 18.0f
 #define PLACEHOLDER_TEXT @"Give Feedback here..."
 
 @interface ViewController () {
     PPBoxView* selectedBox;
+    NSMutableArray* comments;
 }
             
 
@@ -33,12 +46,13 @@
     [self initTextView];
     [self addObservers];
     [self addGestureRecognizers];
+    comments = [NSMutableArray new];
 }
   
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     self.imageScrollView.contentSize = self.imageView.frame.size;
-    
+    self.postButton.enabled = NO;
 }
 
 - (void)addObservers {
@@ -58,7 +72,7 @@
 
 # pragma mark - Resizing Text View Methods
 - (void) initTextView {
-    self.textView = [[HPGrowingTextView alloc] initWithFrame:CGRectMake(X_COMMENT_OFFSET, Y_COMMENT_OFFSET, COMMENT_WIDTH, COMMENT_MIN_HEIGHT)];
+    self.textView = [[HPGrowingTextView alloc] initWithFrame:CGRectMake(X_COMMENT_OFFSET, Y_COMMENT_OFFSET, COMMENT_WIDTH, 0.0f)];
     self.textView.isScrollable = NO;
     self.textView.contentInset = UIEdgeInsetsMake(0, 5, 0, 5);
     self.textView.minNumberOfLines = 1;
@@ -70,17 +84,14 @@
     self.textView.placeholder = PLACEHOLDER_TEXT;
     [self.postCommentContainer addSubview:self.textView];
 
-    self.textView.internalTextView.layer.borderWidth = 1.0f;
+    self.textView.internalTextView.layer.borderWidth = .6f;
     self.textView.internalTextView.layer.borderColor = [[UIColor grayColor] CGColor];
     self.textView.internalTextView.layer.backgroundColor = [UIColor whiteColor].CGColor;
     self.textView.internalTextView.clipsToBounds = YES;
     self.textView.internalTextView.layer.cornerRadius = 10.0f;
 }
 
-
-
-- (void)growingTextView:(HPGrowingTextView *)growingTextView willChangeHeight:(float)height
-{
+- (void) growingTextView:(HPGrowingTextView *) growingTextView willChangeHeight:(float) height {
     float diff = (growingTextView.frame.size.height - height);
     
     CGRect r = self.postCommentContainer.frame;
@@ -89,6 +100,15 @@
     self.postCommentContainer.frame = r;
 
 }
+
+- (void) growingTextViewDidChange:(HPGrowingTextView *) growingTextView {
+    if ([growingTextView.text isEqualToString:@""]) {
+        self.postButton.enabled = NO;
+    } else {
+        self.postButton.enabled = YES;
+    }
+}
+
 
 #pragma mark - Keyboard Hiding and Showing
 - (void) keyboardWillShow:(NSNotification *) aNotification {
@@ -116,6 +136,15 @@
         [self.view layoutIfNeeded];
     }];
 }
+
+/* Dismiss Keyboard */
+- (IBAction)dragPostCommentContainer:(id)sender {
+    CGPoint vectorVelocity = [sender velocityInView:self.mainView];
+    if (vectorVelocity.y > 0) {
+        [self.view endEditing:YES];
+    }
+}
+
 
 #pragma mark - Gesture recognizer delegate
 
@@ -168,6 +197,14 @@
 
 #pragma mark - Post Comment Methods
 - (IBAction)tapPostComment:(id)sender {
+    [comments addObject:self.textView.text];
+    NSLog(@"comments %@", comments);
+    [self didPostComment];
+}
+
+- (void) didPostComment {
+    self.textView.text = @"";
+    [self.view endEditing:YES];
 }
 
 @end
