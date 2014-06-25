946c2fb4c55ca59dbebda0e82dce0f3446613a51
Hani Sharabash <hanibash@gmail.com>
2014-06-24 17:31:36 -0700

    Tap on image adds a box. Selected box has marching ants.

---
+++ PaperPlanesV0.2.0/PPBoxView.m
@@ -0,0 +1,105 @@
+//
+//  PPBoxView.m
+//  PaperPlanesV0.2.0
+//
+//  Created by lux on 6/24/14.
+//  Copyright (c) 2014 Learning Public. All rights reserved.
+//
+
+#import "PPBoxView.h"
+
+@interface PPBoxView () {
+    CAShapeLayer* boxLayer;
+}
+
+@end
+
+#define BOX_DEFAULT_WIDTH 30.0f
+
+@implementation PPBoxView
+
+#pragma mark - Initialization
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self.layer addSublayer:[self boxLayer]];
+        [self setColor:self.tintColor];
+        self.opaque = NO;
+    }
+    return self;
+}
+
+#pragma mark - Drawing
+
+- (void) drawRect:(CGRect)rect {
+    UIBezierPath *rectPath = [UIBezierPath bezierPathWithRect:rect];
+    boxLayer.path = rectPath.CGPath;
+}
+
+#pragma mark - Animation
+
+- (void) marchingAnts:(BOOL)turnOn {
+    float currentLineDashPhase = [boxLayer.presentationLayer lineDashPhase];
+    if (turnOn) {
+        float endLineDashPhase = 10.0f;
+        float duration = 0.75f;
+        CABasicAnimation *marchingAntsAnimation = [CABasicAnimation animationWithKeyPath:@"lineDashPhase"];
+        [marchingAntsAnimation setFromValue:[NSNumber numberWithFloat:0.0f]];
+        [marchingAntsAnimation setToValue:[NSNumber numberWithFloat:endLineDashPhase]];
+        [marchingAntsAnimation setDuration:duration];
+        [marchingAntsAnimation setRepeatCount:10000];
+        // This will start the dashes to animate from the place they last stopped, rather than resetting to phase 0
+        marchingAntsAnimation.timeOffset = (currentLineDashPhase / endLineDashPhase) * duration;
+        [boxLayer addAnimation:marchingAntsAnimation forKey:@"linePhase"];
+    } else {
+        // Sets the model layer to match the presentation layer
+        [boxLayer setLineDashPhase:currentLineDashPhase];
+        [boxLayer removeAnimationForKey:@"linePhase"];
+    }
+}
+
+- (void) toggleMarchingAnts {
+    if ([boxLayer animationForKey:@"linePhase"]) {
+        [self marchingAnts:TRUE];
+    } else {
+        [self marchingAnts:FALSE];
+    }
+}
+
+#pragma mark - Layers
+
+- (CAShapeLayer*) boxLayer {
+    boxLayer = [CAShapeLayer layer];
+    [boxLayer setFillColor:[UIColor clearColor].CGColor];
+    [boxLayer setLineWidth:2.0f];
+    [boxLayer setLineJoin:kCALineCapSquare];
+    [boxLayer setLineDashPattern:[NSArray arrayWithObjects:[NSNumber numberWithInt:5], [NSNumber numberWithInt:5], nil]];
+    
+    return boxLayer;
+}
+
+#pragma mark - Convenience methods
+
+- (void) setColor:(UIColor*) color {
+    [boxLayer setStrokeColor:color.CGColor];
+}
+
++ (PPBoxView*) boxViewCenteredAtPoint: (CGPoint) point {
+    CGSize size = CGSizeMake(BOX_DEFAULT_WIDTH, BOX_DEFAULT_WIDTH);
+    return [self boxViewAtPoint:CGPointMake(point.x - size.width / 2, point.y - size.height / 2) withSize:size];
+}
+
++ (PPBoxView*) boxViewAtPoint: (CGPoint) point {
+    return [self boxViewAtPoint:point withSize:CGSizeMake(BOX_DEFAULT_WIDTH, BOX_DEFAULT_WIDTH)];
+}
+
++ (PPBoxView*) boxViewAtPoint: (CGPoint) point withSize: (CGSize) size {
+    CGRect boxRect = CGRectMake(point.x, point.y, size.width, size.height);
+    return [[self alloc] initWithFrame:boxRect];
+}
+
+
+
+@end
--- PaperPlanesV0.2.0/ViewController.m
+++ PaperPlanesV0.2.0/ViewController.m
@@ -7,22 +7,84 @@
 //
 
 #import "ViewController.h"
+#import "PPBoxView.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    PPBoxView* selectedBox;
+}
             
 
 @end
 
 @implementation ViewController
 
+#pragma mark - initialization code
+
+- (void) viewDidLoad {
+    [super viewDidLoad];
+    [self addGestureRecognizers];
+}
+
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    self.imageScrollView.contentSize = self.imageView.frame.size;    
+    self.imageScrollView.contentSize = self.imageView.frame.size;
 }
+
+- (void) addGestureRecognizers {
+    UITapGestureRecognizer* tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapImageHandler:)];
+    [self.imageView addGestureRecognizer:tapRecognizer];
+}
+
+#pragma mark - Gesture recognizer delegate
+
+- (void) tapImageHandler: (UITapGestureRecognizer *) gesture {
+    CGPoint touchPoint = [gesture locationInView:gesture.view];
+    PPBoxView* touchedBox = [self getTouchedBox:gesture];
+    if (touchedBox == nil) {
+        touchedBox = [PPBoxView boxViewCenteredAtPoint:touchPoint];
+        [gesture.view addSubview:touchedBox];
+    }
+
+    [self selectBox:touchedBox];
+}
+
+- (UIView*) getHitView: (UIGestureRecognizer *) gesture {
+    CGPoint touchPoint = [gesture locationInView:gesture.view];
+    return [gesture.view hitTest:touchPoint withEvent:nil];
+}
+
+#pragma mark - Box methods
+
+- (void) deselect: (PPBoxView*) box {
+    [box marchingAnts:FALSE];
+    selectedBox = nil;
+}
+
+- (void) select: (PPBoxView*) box {
+    [box marchingAnts:TRUE];
+    selectedBox = box;
+}
+
+- (void) selectBox: (PPBoxView*) box {
+    [self deselect:selectedBox];
+    [self select:box];
+}
+
+- (PPBoxView*) getTouchedBox: (UIGestureRecognizer*) gesture {
+    UIView *hitView = [self getHitView:gesture];
+    if ([hitView isKindOfClass:[PPBoxView class]]) {
+        return (PPBoxView*)hitView;
+    }
+    return nil;
+}
+
+#pragma mark - Scroll view delegate
 
 - (UIView *) viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return self.imageView;
 }
+
+#pragma mark -
 
 
 
