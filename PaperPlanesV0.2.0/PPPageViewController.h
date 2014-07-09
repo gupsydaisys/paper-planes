993497c97797aea8997054c949216489fe38d7a0
Hani Sharabash <hanibash@gmail.com>
2014-07-09 10:36:21 -0700

    Snapchat style camera mode + Paging between view controllers

---
+++ PaperPlanesV0.2.0/PPPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPPageViewController.h
+//  PaperPlanesV0.2.0
+//
+//  Created by lux on 7/8/14.
+//  Copyright (c) 2014 Learning Public. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPPageViewController : UIPageViewController <UIPageViewControllerDataSource>
+
+@end
